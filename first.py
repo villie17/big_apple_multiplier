d7fd9781849da599465fdd85f4aad561a7690166
Waleed Mansoor <waleed.mansoor@ascent.ai>
2019-12-12 23:19:16 +0900

    Added test for SemanticAPI
    - Added test query and restructured to have functions

--- first.py
+++ first.py
@@ -1,16 +1,33 @@
 import requests
 import json
 
-resp = requests.get('https://api.nytimes.com/svc/archive/v1/2019/11.json?api-key=')
+KEY = ''
 
-if resp.status_code != 200:
-    # This means something went wrong.
-    raise ApiError('GET  {}'.format(resp.status_code))
+def monthly_summary():
+    resp = requests.get('https://api.nytimes.com/svc/archive/v1/2019/11.json?api-key=' + KEY)
 
-js = resp.json();
-hits = js["response"]["meta"]["hits"]
-print ("Number of hits: ", hits)
-print ("Response:",  hits)
-docs = js["response"]["docs"]
-for d in docs:
-    print(d["abstract"])
+    if resp.status_code != 200:
+        # This means something went wrong.
+        raise ApiError('GET  {}'.format(resp.status_code))
+
+    js = resp.json();
+    hits = js["response"]["meta"]["hits"]
+    print ("Number of hits: ", hits)
+    print ("Response:",  hits)
+    docs = js["response"]["docs"]
+    for d in docs:
+        print(d["abstract"])
+
+def semantic_api():
+    resp = requests.get("http://api.nytimes.com/svc/semantic/v2/concept/search.json?query=Pakistan&concept_type=nytd_geo&api-key="
+ + KEY)
+
+    if resp.status_code != 200:
+            # This means something went wrong.
+            print("Error: {}".format(resp.status_code))
+    js = resp.json();
+    print ("Response: ", js)
+
+if __name__ == "__main__":
+    semantic_api()
+
